ba6fda935e600662d1f2c187719f58eae88d1c7d
TonNomGitHub <TonEmailGitHub>
2026-05-22 02:05:24 +0200

    clean

--- android/app/src/main/kotlin/com/example/simpv_system/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.simpv_system
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/theme.dart
+++
@@ -1,3 +0,0 @@
-export 'core/theme/app_theme.dart';
-export 'core/theme/app_typography.dart';
-export 'core/theme/app_spacing.dart';
